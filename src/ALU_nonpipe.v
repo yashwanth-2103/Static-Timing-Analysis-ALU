3dc24995f2c3cae1c5b76aac5de92cfaa016e5b7
yashwanth-2103 <ee23b136@smail.iitm.ac.in>
2025-10-05 02:27:43 +0530

    Update ALU_nonpipe.v

--- src/ALU_nonpipe.v
+++ src/ALU_nonpipe.v
@@ -1,1 +1,16 @@
+// 8-bit Non-Pipelined ALU
+module ALU_nonpipe (
+    input  [7:0] A, B,
+    input  [1:0] OP,       // 00: Add, 01: Sub, 10: AND, 11: OR
+    output reg [7:0] Y
+);
+    always @(*) begin
+        case(OP)
+            2'b00: Y = A + B;   // Adder delay
+            2'b01: Y = A - B;   // Subtractor delay
+            2'b10: Y = A & B;   // AND delay
+            2'b11: Y = A | B;   // OR delay
+        endcase
+    end
+endmodule
 
